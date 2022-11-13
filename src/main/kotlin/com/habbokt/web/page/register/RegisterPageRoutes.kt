2c994b4b6ac970ab8828630c4db74619dfb4b6b9
Jordan <personal.jordanabraham@gmail.com>
2022-11-13 01:53:50 -0500

    Add getCancelRegister endpoint.

--- src/main/kotlin/com/habbokt/web/page/register/RegisterPageRoutes.kt
+++ src/main/kotlin/com/habbokt/web/page/register/RegisterPageRoutes.kt
@@ -1,13 +1,18 @@
 package com.habbokt.web.page.register
 
 import com.habbokt.web.inject
+import com.habbokt.web.session.CaptchaSession
+import com.habbokt.web.session.RegistrationSession
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
+import io.ktor.server.sessions.clear
 import io.ktor.server.sessions.get
+import io.ktor.server.sessions.sessions
 import io.ktor.server.sessions.set
 
 /**
@@ -20,6 +25,7 @@ fun Application.installRegisterPageRoutes() {
     routing {
         getRegisterPage()
         postRegisterPage()
+        getCancelRegister()
     }
 }
 
@@ -32,5 +38,14 @@ private fun Route.getRegisterPage() {
 private fun Route.postRegisterPage() {
     post("/register") {
         registerPageService.respondRegistration(call)
+    }
+}
+
+private fun Route.getCancelRegister() {
+    get("/register/cancel") {
+        val sessions = call.sessions
+        sessions.clear<CaptchaSession>()
+        sessions.clear<RegistrationSession>()
+        call.respondRedirect("/") // Homepage
     }
 }
