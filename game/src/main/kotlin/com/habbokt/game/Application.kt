b14d2de430192511846283b55eded7bad8b26dd1
Jordan <personal.jordanabraham@gmail.com>
2022-11-15 00:19:15 -0500

    Update Application.kt.

--- game/src/main/kotlin/com/habbokt/game/Application.kt
+++ game/src/main/kotlin/com/habbokt/game/Application.kt
@@ -7,6 +7,7 @@ import io.ktor.network.sockets.openWriteChannel
 import io.ktor.server.application.Application
 import io.ktor.server.application.log
 import java.util.concurrent.Executors
+import kotlin.math.pow
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
@@ -33,12 +34,60 @@ fun Application.module() {
         while (true) {
             val socket = server.accept()
 
+            // TODO
             val readChannel = socket.openReadChannel()
             val writeChannel = socket.openWriteChannel()
 
             launch(Dispatchers.IO) {
+                readChannel.awaitContent()
+                val numBytes = readChannel.availableForRead
 
+                log.info("ReadChannel NumBytes=$numBytes")
+
+                if (numBytes < 5) {
+                    readChannel.awaitContent()
+                }
+
+                val size = decode(ByteArray(3) { readChannel.readByte() })
+                if (readChannel.availableForRead < size) {
+                    readChannel.awaitContent()
+                }
+
+                println(String(encode(206, 2)))
+
+                if (size >= 0) {
+                    val buffer = readChannel.readPacket(size)
+                    val header = String(ByteArray(2) { buffer.readByte() })
+                    val id = decode(header.toByteArray())
+
+                    log.info("Incoming: Header=$header, Id=$id")
+
+                    if (id == 206 && header == "CN") { // InitDiffieHandshakeMessageComposer
+                        log.info("InitDiffieHandshakeMessageComposer")
+                        println(String(encode(206, 2)) == "CN") // Encoding works.
+                        println(readChannel.availableForRead == 0) // All read.
+                    }
+                }
             }
         }
     }
+}
+
+fun encode(id: Int, size: Int): ByteArray {
+    val encodedArray = ByteArray(size)
+    for (index in 1..size) {
+        val k = (size - index) * 6
+        encodedArray[index - 1] = (0x40 + (id shr k and 0x3f)).toByte()
+    }
+    return encodedArray
+}
+
+fun decode(encodedArray: ByteArray): Int {
+    var value = 0
+    for ((j, k) in encodedArray.indices.reversed().withIndex()) {
+        var x = encodedArray[k] - 0x40
+        if (j > 0) x *= 64.0.pow(j.toDouble()).toInt()
+        value += x
+    }
+    return value
 }
