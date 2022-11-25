3bafd4019597f1790d40762d4a52a69218033d45
Jordan <personal.jordanabraham@gmail.com>
2022-11-25 07:32:45 -0500

    Annotate BlankPage Singleton.

--- page/src/main/kotlin/com/habbokt/page/BlankPage.kt
+++ page/src/main/kotlin/com/habbokt/page/BlankPage.kt
@@ -1,11 +1,13 @@
 package com.habbokt.page
 
+import com.google.inject.Singleton
 import io.ktor.http.Parameters
 import io.ktor.server.sessions.CurrentSession
 
 /**
  * @author Jordan Abraham
  */
+@Singleton
 class BlankPage : Page<BlankTemplate> {
     override suspend fun template(sessions: CurrentSession, parameters: Parameters): Template<BlankTemplate> = Template { BlankTemplate }
 }
