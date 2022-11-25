6ba9deb05f9f99fd91a6c394859692da247e9b88
Jordan <personal.jordanabraham@gmail.com>
2022-11-25 08:17:23 -0500

    Add common api package.

---
+++ api/src/main/kotlin/com/habbokt/api/common/base64.kt
@@ -0,0 +1,7 @@
+package com.habbokt.api.common
+
+/**
+ * @author Jordan Abraham
+ */
+fun ByteArray.base64(): Int = foldIndexed(0) { index, sum, x -> sum + ((x - 0x40) shl 6 * (size - 1 - index)) }
+fun Int.base64(size: Int): ByteArray = ByteArray(size) { (((this shr 6 * (size - 1 - it)) and 0x3f) + 0x40).toByte() }
---
+++ api/src/main/kotlin/com/habbokt/api/common/vl64.kt
@@ -0,0 +1,45 @@
+package com.habbokt.api.common
+
+import kotlin.math.abs
+
+/**
+ * @author Jordan Abraham
+ */
+fun Int.vl64(): ByteArray {
+    val vlEncoded = ByteArray(6)
+
+    var size = 1
+    var absoluteValue = abs(this)
+
+    vlEncoded[0] = (64 + (absoluteValue and 3)).toByte()
+
+    absoluteValue = absoluteValue shr 2
+
+    while (absoluteValue != 0) {
+        size++
+        vlEncoded[size - 1] = (64 + (absoluteValue and 0x3f)).toByte()
+        absoluteValue = absoluteValue shr 6
+    }
+
+    vlEncoded[0] = (vlEncoded[0].toInt() or (size shl 3) or if (this >= 0) 0 else 4).toByte()
+
+    return ByteArray(size).apply {
+        System.arraycopy(vlEncoded, 0, this, 0, size)
+    }
+}
+
+fun ByteArray.vl64(): Int {
+    val negative = this[0].toInt() and 4 == 4
+    val size = this[0].toInt() shr 3 and 7
+
+    var result = this[0].toInt() and 3
+
+    var shiftAmount = 2
+
+    for (b in 1 until size) {
+        result = result or (this[b].toInt() and 0x3f shl shiftAmount)
+        shiftAmount = 2 + 6 * b
+    }
+
+    return if (negative) -result else result
+}
