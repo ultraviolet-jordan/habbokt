2b4117e5a682ce4687e1f790aba4b8931d704eaf
Jordan <personal.jordanabraham@gmail.com>
2022-11-25 06:37:33 -0500

    Reimplement IndexPageService.

--- page/src/main/kotlin/com/habbokt/page/index/IndexPageService.kt
+++ page/src/main/kotlin/com/habbokt/page/index/IndexPageService.kt
@@ -2,10 +2,13 @@ package com.habbokt.page.index
 
 import com.google.inject.Inject
 import com.google.inject.Singleton
-import com.habbokt.page.Page
 import com.habbokt.page.PageService
+import com.habbokt.session.UserSession
 import com.habbokt.templating.Compiler
 import io.ktor.server.application.ApplicationCall
+import io.ktor.server.response.respondRedirect
+import io.ktor.server.sessions.get
+import io.ktor.server.sessions.sessions
 
 /**
  * @author Jordan Abraham
@@ -16,6 +19,15 @@ class IndexPageService @Inject constructor(
     compiler: Compiler
 ) : PageService<IndexPage>(page, compiler) {
     override suspend fun handleGetRequest(call: ApplicationCall) {
+        val sessions = call.sessions
+
+        val userSession = sessions.get<UserSession>()
+        // Have to hard check this since index page is not an authenticated page.
+        if (userSession != null && userSession.authenticated) {
+            call.respondRedirect("/me")
+            return
+        }
+
         call.respondHtmlPage()
     }
 }
