99380f866a750f4b63f1afc3424d74b974b1d5c1
Jordan <personal.jordanabraham@gmail.com>
2022-11-27 13:36:10 -0500

    Refactor VersionCheckHandler.

--- packet/src/main/kotlin/com/habbokt/packet/DisconnectReasonPacket.kt
+++ packet/src/main/kotlin/com/habbokt/packet/DisconnectReasonPacket.kt
@@ -1,11 +1,10 @@
 package com.habbokt.packet
 
-import com.habbokt.api.client.DisconnectReason
 import com.habbokt.api.packet.Packet
 
 /**
  * @author Jordan Abraham
  */
 data class DisconnectReasonPacket(
-    val reason: DisconnectReason
+    val reason: Int
 ) : Packet
--- packet/src/main/kotlin/com/habbokt/packet/handler/handshake/VersionCheckHandler.kt
+++ packet/src/main/kotlin/com/habbokt/packet/handler/handshake/VersionCheckHandler.kt
@@ -1,7 +1,7 @@
 package com.habbokt.packet.handler.handshake
 
 import com.google.inject.Singleton
-import com.habbokt.api.client.DisconnectReason
+import com.habbokt.api.client.DisconnectReason.Disconnect
 import com.habbokt.packet.DisconnectReasonPacket
 import com.habbokt.packet.VersionCheckPacket
 import com.habbokt.packet.handler.Handler
@@ -12,21 +12,13 @@ import com.habbokt.packet.handler.HandlerListener
  */
 @Singleton
 class VersionCheckHandler : HandlerListener<VersionCheckPacket>(Handler {
-    if (clientVersionId != 401) {
-        // TODO Validate this against the real file instead of hard coded int.
-        it.writePacket(DisconnectReasonPacket(DisconnectReason.Disconnect))
-        return@Handler
-    }
-
-    if (clientUrl != "") {
-        // TODO Figure out how to populate this from the client.
-        it.writePacket(DisconnectReasonPacket(DisconnectReason.Disconnect))
-        return@Handler
-    }
-
-    if (!clientVarsUrl.startsWith("http://localhost/dcr/v31/gamedata/external_variables.txt")) {
-        // TODO Validate this against the real file instead of hard coded string.
-        it.writePacket(DisconnectReasonPacket(DisconnectReason.Disconnect))
-        return@Handler
+    // TODO Real validation against the local file instead of hard code.
+    when {
+        clientVersionId != 401 -> Disconnect
+        clientUrl != "" -> Disconnect
+        !clientVarsUrl.startsWith("http://localhost/dcr/v31/gamedata/external_variables.txt") -> Disconnect
+        else -> null
+    }?.let { disconnectReason ->
+        it.writePacket(DisconnectReasonPacket(disconnectReason.id))
     }
 })
