75c932d71f1f70637d2e4896822f4444e848cfb7
Jordan <personal.jordanabraham@gmail.com>
2022-11-30 23:57:26 -0500

    Apply hair color.

--- page/src/main/kotlin/com/habbokt/page/habboimaging/avatarimage/AvatarImageService.kt
+++ page/src/main/kotlin/com/habbokt/page/habboimaging/avatarimage/AvatarImageService.kt
@@ -4,11 +4,15 @@ import com.google.inject.Inject
 import com.google.inject.Singleton
 import com.habbokt.page.BlankPage
 import com.habbokt.page.BlankPageService
+import com.habbokt.swf.avatarsbig.AvatarsBigSwf
 import com.habbokt.xml.XMLDocument
 import com.habbokt.xml.draworder.domain.DrawOrder
 import com.habbokt.xml.figuredata.domain.FigureData
 import com.habbokt.xml.partsets.domain.PartSets
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
+import io.ktor.server.response.respond
+import java.awt.image.BufferedImage
 
 /**
  * @author Jordan Abraham
@@ -18,7 +22,8 @@ class AvatarImageService @Inject constructor(
     page: BlankPage,
     private val figureDataDocument: XMLDocument<FigureData>,
     private val drawOrderDocument: XMLDocument<DrawOrder>,
-    private val partSetsDocument: XMLDocument<PartSets>
+    private val partSetsDocument: XMLDocument<PartSets>,
+    private val avatarsBigSwf: AvatarsBigSwf
 ) : BlankPageService(page) {
     override suspend fun handleGetRequest(call: ApplicationCall) {
         val parameters = call.parameters
@@ -47,19 +52,45 @@ class AvatarImageService @Inject constructor(
         val drawOrder = drawOrderDocument.xml
         val partSets = partSetsDocument.xml
 
-        val hairName = hair(figureData, hair, direction)
+        val hairImage = hair(figureData, hair) ?: return call.respond(HttpStatusCode.BadRequest)
+        call.respondPng(hairImage) // TODO
     }
 
-    private fun hair(figureData: FigureData, hair: String?, direction: String?): String {
+    private fun hair(figureData: FigureData, hair: String?): BufferedImage? {
         val split = hair?.split("-")
         val part = split?.get(0)
         val setId = split?.get(1)
         val colorId = split?.get(2)
 
-        val swfPart = figureData.sets.setTypes.firstOrNull { it.type == part }?.sets?.firstOrNull { it.id == setId?.toInt() }?.part
-        val swfPartId = swfPart?.id
-        val swfPartType = swfPart?.type
+        val xmlSetType = figureData.sets.setTypes.firstOrNull { it.type == part }
+        val xmlPart = xmlSetType?.sets?.firstOrNull { it.id == setId?.toInt() }?.part
+        val xmlPartId = xmlPart?.id
+        val xmlPartType = xmlPart?.type
+        val xmlColor = figureData.colors.palettes.firstOrNull { it.id == xmlSetType?.paletteId }?.colors?.firstOrNull { it.id == colorId?.toInt() }
 
-        return "h_std_${part}_${swfPartId}_2_0"
+        val swfString = "h_std_${part}_${xmlPartId}_2_0"
+        val swfHairObject = avatarsBigSwf.data.firstOrNull { it.export.name == swfString }
+        val swfHairImage = swfHairObject?.image?.bufferedImage ?: swfHairObject?.image2?.bufferedImage
+
+        return swfHairImage?.applyHexColor(xmlColor?.hex)
+    }
+
+    private fun BufferedImage.applyHexColor(hex: String?): BufferedImage {
+        val overlay = java.awt.Color.decode("#$hex")
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                val currPixelColor = java.awt.Color(getRGB(x, y), true)
+                if (currPixelColor.alpha > 0) {
+                    val newPixelColor = java.awt.Color(
+                        overlay.red * currPixelColor.red / 255,
+                        overlay.green * currPixelColor.green / 255,
+                        overlay.blue * currPixelColor.blue / 255,
+                        currPixelColor.alpha
+                    )
+                    setRGB(x, y, newPixelColor.rgb)
+                }
+            }
+        }
+        return this
     }
 }
