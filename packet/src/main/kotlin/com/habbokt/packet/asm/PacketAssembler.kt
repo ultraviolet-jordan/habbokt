373fec9ac4fe304837bd17bc5f02d1dd45b8f755
Jordan <personal.jordanabraham@gmail.com>
2022-12-02 16:48:13 -0500

    Remove unused code.

--- packet/src/main/kotlin/com/habbokt/packet/asm/PacketAssembler.kt
+++ packet/src/main/kotlin/com/habbokt/packet/asm/PacketAssembler.kt
@@ -1,19 +1,10 @@
 package com.habbokt.packet.asm
 
 import com.habbokt.api.packet.Packet
-import java.lang.reflect.ParameterizedType
 
 /**
  * @author Jordan Abraham
  */
 open class PacketAssembler<out P : Packet>(
     val assembler: Assembler<@UnsafeVariance P>
-) {
-    /**
-     * Weird solution of checking if a packet is of type of this assembler since assemblers are basically keyed by class type.
-     */
-//    fun typeOf(packet: @UnsafeVariance P): Boolean = (this::class.java.genericSuperclass as ParameterizedType)
-//        .actualTypeArguments
-//        .firstOrNull()
-//        ?.typeName == packet::class.java.typeName
-}
+)
