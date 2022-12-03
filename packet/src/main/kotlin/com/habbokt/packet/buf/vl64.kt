234256846b5d79c3908b96ce94eda0c5aa4e74e8
Jordan <personal.jordanabraham@gmail.com>
2022-12-03 01:56:51 -0500

    Refactor vl64.

--- packet/src/main/kotlin/com/habbokt/packet/buf/vl64.kt
+++ packet/src/main/kotlin/com/habbokt/packet/buf/vl64.kt
@@ -5,41 +5,40 @@ import kotlin.math.abs
 /**
  * @author Jordan Abraham
  */
-fun Int.vl64(): ByteArray {
-    val vlEncoded = ByteArray(6)
+fun Int.vl64(): ByteArray = vl64TailRec()
+fun ByteArray.vl64(): Int = vl64TailRec()
 
-    var size = 1
-    var absoluteValue = abs(this)
-
-    vlEncoded[0] = (64 + (absoluteValue and 3)).toByte()
-
-    absoluteValue = absoluteValue shr 2
-
-    while (absoluteValue != 0) {
-        size++
-        vlEncoded[size - 1] = (64 + (absoluteValue and 0x3f)).toByte()
-        absoluteValue = absoluteValue shr 6
+private tailrec fun Int.vl64TailRec(
+    size: Int = 1,
+    absoluteValue: Int = abs(this) shr 2,
+    result: ByteArray = ByteArray(size = 6).apply { this[0] = (64 + (abs(this@vl64TailRec) and 3)).toByte() }
+): ByteArray {
+    if (absoluteValue == 0) {
+        return result.apply {
+            this[0] = (this[0].toInt() or (size shl 3) or if (this@vl64TailRec >= 0) 0 else 4).toByte()
+        }.copyInto(
+            destination = ByteArray(size),
+            destinationOffset = 0,
+            startIndex = 0,
+            endIndex = size
+        )
     }
-
-    vlEncoded[0] = (vlEncoded[0].toInt() or (size shl 3) or if (this >= 0) 0 else 4).toByte()
-
-    return ByteArray(size).apply {
-        System.arraycopy(vlEncoded, 0, this, 0, size)
-    }
+    return vl64TailRec(
+        size = size + 1,
+        absoluteValue = absoluteValue shr 6,
+        result = result.apply { this[size] = (64 + (absoluteValue and 0x3f)).toByte() }
+    )
 }
 
-fun ByteArray.vl64(): Int {
-    val negative = this[0].toInt() and 4 == 4
-    val size = this[0].toInt() shr 3 and 7
-
-    var result = this[0].toInt() and 3
-
-    var shiftAmount = 2
-
-    for (b in 1 until size) {
-        result = result or (this[b].toInt() and 0x3f shl shiftAmount)
-        shiftAmount = 2 + 6 * b
-    }
-
-    return if (negative) -result else result
+private tailrec fun ByteArray.vl64TailRec(
+    result: Int = this[0].toInt() and 3,
+    shiftAmount: Int = 2,
+    position: Int = 1
+): Int {
+    if (position == this[0].toInt() shr 3 and 7) return if (this[0].toInt() and 4 == 4) -result else result
+    return vl64TailRec(
+        result = result or (this[position].toInt() and 0x3f shl shiftAmount),
+        shiftAmount = 2 + 6 * position,
+        position = position + 1
+    )
 }
