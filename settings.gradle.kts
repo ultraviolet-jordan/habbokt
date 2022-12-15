2007f9fcd5163d46973a091ee3010040fb0fed63
Jordan <personal.jordanabraham@gmail.com>
2022-12-14 19:08:43 -0500

    Update dependencies.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,11 @@ dependencyResolutionManagement {
         create("deps") {
             // Dependency versions.
             version("kotlin", "1.7.21")
-            version("ktor", "2.1.3")
+            version("ktor", "2.2.1")
             version("versions", "0.44.0")
             version("pebble", "3.2.0")
             version("ktlint", "11.0.0")
-            version("slf4j", "2.0.5")
+            version("slf4j", "2.0.6")
             version("simplecaptcha", "1.2.2")
             version("argon2", "2.11")
             version("exposed", "0.41.1")
