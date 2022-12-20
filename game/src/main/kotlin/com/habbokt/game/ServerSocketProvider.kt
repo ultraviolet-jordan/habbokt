a4d0d3f2924ee67fba58f11562dacddaeebcb179
Jordan <personal.jordanabraham@gmail.com>
2022-12-20 08:59:58 -0500

    Update ServerSocketProvider.

--- game/src/main/kotlin/com/habbokt/game/ServerSocketProvider.kt
+++ game/src/main/kotlin/com/habbokt/game/ServerSocketProvider.kt
@@ -4,12 +4,14 @@ import com.google.inject.Inject
 import com.google.inject.Provider
 import com.google.inject.Singleton
 import io.ktor.network.selector.ActorSelectorManager
+import io.ktor.network.selector.SelectorManager
 import io.ktor.network.sockets.ServerSocket
 import io.ktor.network.sockets.aSocket
 import io.ktor.server.application.ApplicationEnvironment
 import io.ktor.server.application.host
 import io.ktor.server.application.port
 import java.util.concurrent.Executors
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
 
 /**
@@ -19,10 +21,7 @@ import kotlinx.coroutines.asCoroutineDispatcher
 class ServerSocketProvider @Inject constructor(
     private val environment: ApplicationEnvironment
 ) : Provider<ServerSocket> {
-    private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
-    private val selector = ActorSelectorManager(dispatcher)
-
-    override fun get(): ServerSocket = aSocket(selector).tcp().bind(environment.config.host, environment.config.port) {
+    override fun get(): ServerSocket = aSocket(SelectorManager(Dispatchers.IO)).tcp().bind(environment.config.host, environment.config.port) {
         reuseAddress = true
     }
 }
