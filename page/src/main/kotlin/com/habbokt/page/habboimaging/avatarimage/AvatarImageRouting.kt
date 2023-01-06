624cbf70a94d04300c95f6a7c4ba33a8232241c3
Jordan <personal.jordanabraham@gmail.com>
2023-01-06 02:24:58 -0500

    Authenticate habbo-imaging endpoint.

--- page/src/main/kotlin/com/habbokt/page/habboimaging/avatarimage/AvatarImageRouting.kt
+++ page/src/main/kotlin/com/habbokt/page/habboimaging/avatarimage/AvatarImageRouting.kt
@@ -2,9 +2,11 @@ package com.habbokt.page.habboimaging.avatarimage
 
 import com.google.inject.Inject
 import com.google.inject.Singleton
+import com.habbokt.page.Authentications
 import com.habbokt.page.PageRouting
 import com.habbokt.page.Route
 import io.ktor.server.application.call
+import io.ktor.server.auth.authenticate
 import io.ktor.server.routing.get
 
 /**
@@ -14,7 +16,9 @@ import io.ktor.server.routing.get
 class AvatarImageRouting @Inject constructor(
     private val service: AvatarImageService
 ) : PageRouting(Route {
-    get("/habbo-imaging/avatarimage") {
-        service.handleGetRequest(call)
+    authenticate(Authentications.User) {
+        get("/habbo-imaging/avatarimage") {
+            service.handleGetRequest(call)
+        }
     }
 })
