c4842af342e8ffc6db6284d895793b5981441e5f
Jordan <personal.jordanabraham@gmail.com>
2023-01-31 23:38:20 -0500

    Update BlankPageService.

--- page/src/main/kotlin/com/habbokt/page/BlankPageService.kt
+++ page/src/main/kotlin/com/habbokt/page/BlankPageService.kt
@@ -4,6 +4,6 @@ package com.habbokt.page
  * @author Jordan Abraham
  */
 abstract class BlankPageService<Z : PageRequest, G : ResponseType, P : ResponseType>(
-    val get: (suspend Z.(BlankPage<Z>) -> G)? = null,
-    val post: (suspend Z.(BlankPage<Z>) -> P)? = null
+    get: (suspend Z.(BlankPage<Z>) -> G)? = null,
+    post: (suspend Z.(BlankPage<Z>) -> P)? = null
 ) : PageService<BlankPage<Z>, Z, G, P>(get, post)
