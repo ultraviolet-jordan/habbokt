76858dddd3c44262d315cee7d7131ef820716a69
Jordan <personal.jordanabraham@gmail.com>
2023-04-22 19:58:48 -0400

    Cleanup imports.

--- page/src/main/kotlin/com/habbokt/page/asset/AssetPageRouting.kt
+++ page/src/main/kotlin/com/habbokt/page/asset/AssetPageRouting.kt
@@ -2,11 +2,7 @@ package com.habbokt.page.asset
 
 import com.google.inject.Singleton
 import com.habbokt.page.PageRouting
-import io.ktor.server.http.content.files
-import io.ktor.server.http.content.static
 import io.ktor.server.http.content.staticResources
-import io.ktor.server.http.content.staticRootFolder
-import java.io.File
 
 /**
  * @author Jordan Abraham
